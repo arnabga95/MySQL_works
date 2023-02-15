96caced43e433824448f525f47b9378b945c9b4a
Arnab Garai <119153368+arnabga95@users.noreply.github.com>
2023-02-15 13:34:37 +0530

    Add files via upload

---
+++ Employees/exercise 1.sql
@@ -0,0 +1,3 @@
+# Problem:- SELECT ALL THE INFORMATION FROM THE "SALARIES" TABLE REGARDING CONTRACTS FROM 66000 TO 70000 DOLLARS PER YEAR. 
+#Solution:-
+select * from salaries where salary between 66000 and 70000; 
